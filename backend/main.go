ce419e8527f4f7e410885bfec904714231a7f3fb
kazuki1023 <k.keio12569@gmail.com>
2024-07-19 20:06:58 +0900

    feat: :sparkles: cors設定を追加した

--- backend/main.go
+++ backend/main.go
@@ -24,6 +24,19 @@ func main() {
 		log.Fatalf("failed creating schema resources: %v", err)
 	}
 
+	// CORS設定
+	router.Use(func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(200)
+			return
+		}
+		c.Next()
+	})
+
 	// ルートハンドラの定義
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
